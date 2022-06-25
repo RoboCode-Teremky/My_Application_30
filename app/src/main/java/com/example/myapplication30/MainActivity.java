4d062627eeb28e161cc8c892cebb4fef07025f62
teacher <robocode.teremky.students@gmail.com>
2022-06-26 01:38:58 +0300

    second commit

--- app/src/main/java/com/example/myapplication30/MainActivity.java
+++ app/src/main/java/com/example/myapplication30/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //todo: write some code here
+        //code for old main branch
     }
 }
